23cad570bf049d966554306146997b8f9ab1bb85
zainabshahzad-dev <zainabshahzad842@gmail.com>
2025-12-04 00:07:01 +0500

    Fix error

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/e-plantShopping/',
-  plugins: [react()]
+  plugins: [react()],
+  base: '/e-plantShopping/'   // IMPORTANT
 })
